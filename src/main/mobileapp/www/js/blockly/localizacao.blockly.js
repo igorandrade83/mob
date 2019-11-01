0c044a72afa92416d8905e0d37fc2c75316a8d1a
Testedoc Testesdocs <sugarmand@gmail.com>
2019-11-01 02:51:08 +0000

    Commit alterações.

--- src/main/mobileapp/www/js/blockly/localizacao.blockly.js
+++ src/main/mobileapp/www/js/blockly/localizacao.blockly.js
@@ -20,9 +20,9 @@ window.blockly.js.blockly.Localizacao.linkLocalizacao = function() {
      '6778!2sElevador%20Lacerda!5e0!3m2!1spt-BR!2sbr!4v1571968312917!5m2!1spt-BR!2sbr');
     this.cronapi.screen.changeContent("resumoLocalizacao", 'Localização do Elevador Lacerda.\n', 'false');
   } else if (qs == '3') {
-    this.cronapi.screen.changeContent("titleLocal", 'Elevador', 'false');
+    this.cronapi.screen.changeContent("titleLocal", 'Farol da Barra', 'false');
     this.cronapi.screen.changeAttrValue("iframemap", 'src', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.9611473393684!2d-38.51533258447996!3d-12.974336863381149!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x71604efa5d0086f%3A0x725930e371c1' +
      '6778!2sElevador%20Lacerda!5e0!3m2!1spt-BR!2sbr!4v1571968312917!5m2!1spt-BR!2sbr');
-    this.cronapi.screen.changeContent("resumoLocalizacao", 'Localização Elevador Lacerda.\n', 'false');
+    this.cronapi.screen.changeContent("resumoLocalizacao", 'Farol da Barra', 'false');
   }
 }
